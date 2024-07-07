f8f260de3d607f22dc76d48f58a588e4de64ed58
JeinKh <juliakhezhun@gmail.com>
2024-07-07 23:15:48 +0300

    //++

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,55 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
 
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'vertical',
+//     loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
 
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-});
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
+// });
+
+// (() => {
+//     const mobileMenu = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
+//     const closeMenuLink = document.querySelectorAll('.header-mob-menu-link');
+
+//     const toggleMenu = () => {
+//     const isMenuOpen =
+//         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//         ? 'disableBodyScroll'
+//         : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//     };
+//     closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
+
+//     // Close the mobile menu on wider screens if the device orientation changes
+//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//     });
+// })();
